50f81f8bd996424c4e67bbb4fdd3228375d1e5ac
nikill04 <vasireddynikhil04@gmail.com>
2026-06-25 20:55:51 +0530

    final frontend url enabling cors

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://nikhils-postman.vercel.app",  # replace with your actual Vercel URL
+        "https://nikhil-postman.vercel.app",  # replace with your actual Vercel URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
